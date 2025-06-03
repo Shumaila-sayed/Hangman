a7942aa41405959f868d643b5be8dfe4ce40ae5c
Shumaila <sayedshumaila27@gmail.com>
2025-06-04 01:42:49 +0530

    structure the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-function App() {
+import { useState } from 'react';
+import words from './wordList.json';
+import { HangmanDrawing } from './components/HangmanDrawing';
+import { HangmanWord } from './components/HangmanWord';
+import { Keyboard } from './components/Keyboard';
 
-  return (
-    <h1>HI</h1>
-  )
+function App() {
+	const [wordToGuess, setWordToGuess] = useState(() => {
+		return words[Math.floor(Math.random() * words.length)];
+	});
+	const [guessedLetters, setGuessedLetter] = useState<string[]>([]);
+
+	return (
+		<div
+			style={{
+				maxWidth: '800px',
+				display: 'flex',
+				flexDirection: 'column',
+				gap: '2rem',
+				margin: '0 auto',
+				alignItems: 'center',
+			}}
+		>
+      <div style={{ fontSize: '2rem', textAlign: 'center' }}>Lose Win</div>
+      
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+		</div>
+	);
 }
 
-export default App
+export default App;
